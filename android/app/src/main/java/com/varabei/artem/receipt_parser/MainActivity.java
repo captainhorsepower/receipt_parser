2da517afa97de143c14917f56de005dc9a738ffe
Artem Varabei <captainhorsepower228@gmail.com>
2019-07-17 23:51:59 -0600

    init commit

---
+++ android/app/src/main/java/com/varabei/artem/receipt_parser/MainActivity.java
@@ -0,0 +1,13 @@
+package com.varabei.artem.receipt_parser;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
